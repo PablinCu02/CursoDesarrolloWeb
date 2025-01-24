80eee9e68b8e03ca12fb36094d2f1d28fce4150b
PablinCu02 <90666361+PablinCu02@users.noreply.github.com>
2025-01-24 21:55:48 +0000

    Primer Borrador Juego Adivinanza

---
+++ app.js
@@ -0,0 +1,22 @@
+// Comentario 
+/*6
+DGFGDDFG
+como estas
+*/
+
+let numeroSecreto = 8;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if(numeroUsuario == numeroSecreto){
+            alert("El numero que ingresaste es mayor que el secreto");
+}
+if(numeroUsuario < numeroSecreto){
+            alert("El numero que ingresaste es menor que el secreto")
+}
+if(numeroUsuario == numeroSecreto){
+            alert("El numero que ingresaste es igual al secreto");
+}
+
+
