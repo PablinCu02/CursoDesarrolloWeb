237c55b2c46f9085c564443ae111ee4537b9d300
PablinCu02 <90666361+PablinCu02@users.noreply.github.com>
2025-01-24 23:00:35 +0000

    Agrego numero de intentos

--- app.js
+++ app.js
@@ -5,18 +5,26 @@ como estas
 */
 
 let numeroSecreto = 8;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario=0;
+let intentos=0;
 
-console.log(numeroUsuario);
 
-if(numeroUsuario == numeroSecreto){
-            alert("El numero que ingresaste es mayor que el secreto");
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un numero por favor:");
+
+    console.log(numeroUsuario);
+
+    if(numeroUsuario == numeroSecreto){
+                alert('El numero que ingresaste es igual que el secreto');
+                alert(`El numero de intentos que te llevó fue de ${intentos} intentos`)
+    }else
+        if(numeroUsuario < numeroSecreto)
+                alert('El numero que ingresaste es menor que el secreto')
+       if(numeroUsuario > numeroSecreto)
+                alert('El numero que ingresaste es mayor al secreto');
+        intentos++;  
+
 }
-if(numeroUsuario < numeroSecreto){
-            alert("El numero que ingresaste es menor que el secreto")
-}
-if(numeroUsuario == numeroSecreto){
-            alert("El numero que ingresaste es igual al secreto");
-}
+
 
 
