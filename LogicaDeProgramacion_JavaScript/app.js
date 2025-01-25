8baddbeb33c1351953b740ac63dea77fb7e50820
PablinCu02 <90666361+PablinCu02@users.noreply.github.com>
2025-01-25 01:13:27 +0000

    Juego Terminado

--- LogicaDeProgramacion_JavaScript/app.js
+++ LogicaDeProgramacion_JavaScript/app.js
@@ -1,17 +1,16 @@
-
 /*
 Programa para adivinar un número
 */
 
-let numeroSecreto = 8;
+let numeroSecreto = parseInt(Math.random()*10)+1;
 let numeroUsuario=0;
 let intentos=0; //Llevará el conteo de intentos
+let maxintentos=4;
+
 
 
 while(numeroUsuario != numeroSecreto){ //Diferente de
-    numeroUsuario = prompt("Me indicas un numero por favor:");
-
-    console.log(numeroUsuario);
+    numeroUsuario = prompt("Ingresa un numero entre 1 y 10:");
 
     if(numeroUsuario == numeroSecreto){
                 alert('El numero que ingresaste es igual que el secreto');
@@ -22,6 +21,11 @@ while(numeroUsuario != numeroSecreto){ //Diferente de
        if(numeroUsuario > numeroSecreto)
                 alert('El numero que ingresaste es mayor al secreto');
         intentos++; //El contador de intentos se incrementa en 1
+
+        if(intentos > maxintentos){//Ponemos un limite de intentos
+                alert(`Lo siento, has agotado los ${maxintentos} intentos`)
+                break;
+        }
 
 }
 
